749df18115fae314fd3bb218db0a5c1ff1f4090d
erwilson98 <32907881+erwilson98@users.noreply.github.com>
2018-09-17 17:35:25 -0400

    added the user class

---
+++ app/src/main/java/kantwonskids/donationtrackerg14b/User.java
@@ -0,0 +1,24 @@
+
+public class User {
+
+    // class variables
+    private String username;
+    private String password;
+
+    // getters and setters
+    public void setUsername(String x) {
+        username = x;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String x) {
+        password = x;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
