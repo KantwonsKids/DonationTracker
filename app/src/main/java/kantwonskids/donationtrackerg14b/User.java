5cdd3afef167eeba3b40135293f84bc507073f32
erwilson98 <32907881+erwilson98@users.noreply.github.com>
2018-09-19 12:46:56 -0400

    Updated user class
    javadocs and constructor shit

--- app/src/main/java/kantwonskids/donationtrackerg14b/User.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/User.java
@@ -1,27 +1,59 @@
 package kantwonskids.donationtrackerg14b;
 
 /**
- * A basic user class that contains a username and password
+ * @author Ethan Wilson <ewilson72@gatech.edu>
+ * @version 1.1
+ *
+ * A basic user class
  */
 public class User {
 
-    // class variables
     private String username;
     private String password;
 
-    // getters and setters
+    /**
+     * Creates a new user with a given username and password
+     *
+     * @param u the username
+     * @param p the password
+     */
+    User(String u, String p) {
+        username = u;
+        password = p;
+    }
+
+    /**
+     * Sets the username
+     *
+     * @param x new username
+     */
     public void setUsername(String x) {
         username = x;
     }
 
+    /**
+     * Gets the username
+     *
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the password
+     *
+     * @param x new password
+     */
     public void setPassword(String x) {
         password = x;
     }
 
+    /**
+     * Gets the password
+     *
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
