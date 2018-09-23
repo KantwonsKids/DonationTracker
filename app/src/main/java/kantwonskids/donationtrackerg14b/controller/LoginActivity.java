170754a0a4bf76579cc5eadc6440060fe51c8d7b
Daniel Profili <dprofili@gmail.com>
2018-09-23 02:00:28 -0400

    Add logout button

--- app/src/main/java/kantwonskids/donationtrackerg14b/controller/LoginActivity.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/controller/LoginActivity.java
@@ -52,7 +52,7 @@ public class LoginActivity extends AppCompatActivity {
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "username", "password"
+            "user", "pass"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
