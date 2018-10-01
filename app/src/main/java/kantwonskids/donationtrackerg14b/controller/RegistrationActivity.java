ca8eccce518bbf0896ec15a5f4a386778d75b036
erwilson98 <32907881+erwilson98@users.noreply.github.com>
2018-09-30 20:57:06 -0400

    making the app perfect

--- app/src/main/java/kantwonskids/donationtrackerg14b/controller/RegistrationActivity.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/controller/RegistrationActivity.java
@@ -1,13 +1,75 @@
-package kantwonskids.donationtrackerg14b;
+package kantwonskids.donationtrackerg14b.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import kantwonskids.donationtrackerg14b.R;
+import kantwonskids.donationtrackerg14b.model.*;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+
+    // username and password field values
+    private EditText usernameField;
+    private EditText passwordField;
+    private EditText confirmField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // get references
+        usernameField = findViewById(R.id.registration_usernameField);
+        passwordField = findViewById(R.id.registration_passwordField);
+        confirmField = findViewById(R.id.registration_confirmField);
+
+
+
+        // Attempt to create an account if the account doesn't already exist
+        Button createAccountButton = findViewById(R.id.registration_createAccount_button);
+        createAccountButton.setOnClickListener((view) -> {
+            attemptRegistration();
+        });
+
+
+        // Return to welcome screen if cancel button is pressed
+        Button cancelButton = findViewById(R.id.registration_cancel_button);
+        cancelButton.setOnClickListener((view) -> {
+            Intent intent_cancel = new Intent(this, WelcomeActivity.class);
+            startActivity(intent_cancel);
+        });
+    }
+
+    private void attemptRegistration() {
+        usernameField.setError(null);
+        passwordField.setError(null);
+        confirmField.setError(null);
+
+        String u = usernameField.getText().toString();
+        String p = passwordField.getText().toString();
+        String c = confirmField.getText().toString();
+
+        // get reference to the model
+        Model model = Model.getInstance();
+        // check to see if the username exists in the list
+        if (model._userList.usernameTaken(u)) {
+            usernameField.setError("Username is taken");
+        }
+        else if (!p.equals(c)) {
+            passwordField.setError("Passwords do not match!");
+        }
+        else
+        {
+            model._userList.addUser(new User(u, p));
+            //Model.push(model);
+            Intent intent_welcome = new Intent(this, MainActivity.class);
+            startActivity(intent_welcome);
+        }
+
+
     }
 }
---
+++ app/src/main/java/kantwonskids/donationtrackerg14b/model/Model.java
@@ -0,0 +1,26 @@
+package kantwonskids.donationtrackerg14b.model;
+
+/**
+ * @author Ethan Wilson
+ *
+ *
+ * A singleton instance of the model that controllers
+ * can access
+ */
+public class Model {
+    // instance of the class
+    private static Model _instance = new Model();
+    public static Model getInstance() { return _instance; }
+    public static void push(Model m) { _instance = m; }
+
+    // so that we can skip remaking methods in this class
+    // security is retarded
+    public UserList _userList;
+
+    /**
+     * creates a new model
+     */
+    private Model() {
+        _userList = new UserList();
+    }
+}
