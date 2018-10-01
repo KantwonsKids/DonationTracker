baca84af4e3856d90e7ef403f39960085ccf26a1
Daniel Profili <dprofili@gmail.com>
2018-10-01 18:37:55 -0400

    Add guest class and have user extend

---
+++ app/src/main/java/kantwonskids/donationtrackerg14b/model/Guest.java
@@ -0,0 +1,14 @@
+package kantwonskids.donationtrackerg14b.model;
+
+/**
+ * Represents a guest, who has the following permissions:
+ *     - Can view information at all locations without signing in
+ */
+public class Guest {
+    /**
+     * Constructs a new guest.
+     */
+    public Guest() {
+
+    }
+}
