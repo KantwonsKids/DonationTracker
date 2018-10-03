275bdabf5383ccee49adb2a6bfdd02b1c36116b6
Daniel Profili <dprofili@gmail.com>
2018-10-03 18:38:53 -0400

    Add new user constructors

--- app/src/main/java/kantwonskids/donationtrackerg14b/model/User.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/model/User.java
@@ -13,18 +13,45 @@ public class User extends Guest implements Parcelable {
 
     private String username;
     private String password;
+    private UserRole role;
+    private String location;
 
     /**
-     * Creates a new user with a given username and password
-     *
-     * Has to be public in order to create users outside of package
+     * Creates a new user with a given username, password, role, and assigned location.
+     * The location must be null for all roles but location employees.
      *
      * @param username the username
      * @param password the password
+     * @param role the user's role
+     * @param location the user's location
+     * @throws IllegalArgumentException If the role is not location employee and "location" is anything other than null
      */
-    public User(String username, String password) {
+    public User(String username, String password, UserRole role, String location) {
         this.username = username;
         this.password = password;
+        this.role = role;
+        if (location != null && role != UserRole.LOCATION_EMPLOYEE) {
+            throw new IllegalArgumentException("Only location employees can have assigned" +
+                    "locations");
+        }
+        this.location = location;
+
+    }
+
+    /**
+     * Creates a new user with a given username, password, and role.
+     * If the new user is a location employee, an exception is thrown, as the location must be
+     * specified.
+     *
+     * @param username the username
+     * @param password the password
+     * @param role the user's role
+     */
+    public User(String username, String password, UserRole role) {
+        this(username, password, role, null);
+        if (role == UserRole.LOCATION_EMPLOYEE) {
+            throw new IllegalArgumentException("A location must be specified for location employees.");
+        }
     }
 
     /**
@@ -34,6 +61,8 @@ public class User extends Guest implements Parcelable {
     public User(Parcel in) {
         this.username = in.readString();
         this.password = in.readString();
+        this.location = in.readString();
+        this.role = (UserRole)in.readSerializable();
     }
 
     /**
@@ -96,6 +125,8 @@ public class User extends Guest implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.username);
         dest.writeString(this.password);
+        dest.writeString(this.location);
+        dest.writeSerializable(this.role);
     }
 
     @Override
