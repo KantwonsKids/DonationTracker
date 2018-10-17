28ef616e61ea101c846f1640c5929912cba4b465
Juliana <jpetrillo3@gatech.edu>
2018-10-17 15:39:38 -0400

    Add donation class.

--- app/src/main/java/kantwonskids/donationtrackerg14b/model/Location.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/model/Location.java
@@ -1,12 +1,15 @@
 package kantwonskids.donationtrackerg14b.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Ethan Wilson <ewilson72@gatech.edu>
  * @version 1.2
  *
  * A wrapper that stores all data of a specific donation
  */
-public class DonationData {
+public class Location {
 
     private int key;
     private String name;
@@ -19,13 +22,14 @@ public class DonationData {
     private String type;  // could be an enum
     private String phoneNumber; // String because it has the () and -
     private String website;
+    private List<Donation> donations;
 
     /**
      * Constructor that takes in type-appropriate data
      */
-    public DonationData(int key, String name, double latitude, double longitude, String address,
-                        String city, String state, int zipcode, String type, String phoneNumber,
-                        String website) {
+    public Location(int key, String name, double latitude, double longitude, String address,
+                    String city, String state, int zipcode, String type, String phoneNumber,
+                    String website, List<Donation> donations) {
         this.key = key;
         this.name = name;
         this.latitude = latitude;
@@ -37,14 +41,15 @@ public class DonationData {
         this.type = type;
         this.phoneNumber = phoneNumber;
         this.website = website;
+        this.donations = donations;
     }
 
     /**
      * Constructor that takes in all Strings then automatically casts
      */
-    public DonationData(String key, String name, String latitude, String longitude, String address,
-                        String city, String state, String zipcode, String type, String phoneNumber,
-                        String website) {
+    public Location(String key, String name, String latitude, String longitude, String address,
+                    String city, String state, String zipcode, String type, String phoneNumber,
+                    String website) {
         this.key = Integer.parseInt(key);
         this.name = name;
         this.latitude = Double.parseDouble(latitude);
@@ -56,6 +61,14 @@ public class DonationData {
         this.type = type;
         this.phoneNumber = phoneNumber;
         this.website = website;
+        this.donations = new ArrayList<>();
+    }
+
+    public Location(int key, String name, double latitude, double longitude, String address,
+                    String city, String state, int zipcode, String type, String phoneNumber,
+                    String website) {
+        this(key, name, latitude, longitude, address, city, state, zipcode,
+                type, phoneNumber, website, new ArrayList<>());
     }
 
     public int getKey() {
@@ -149,5 +162,21 @@ public class DonationData {
     @Override
     public String toString() {
         return this.name;
+    }
+
+    public List<Donation> getDonations() {
+        return donations;
+    }
+
+    public void setDonations(List<Donation> donations) {
+        this.donations = donations;
+    }
+
+    public void addDonation(Donation donation) {
+        donations.add(donation);
+    }
+
+    public void addDonation(List<Donation> donations) {
+        this.donations.addAll(donations);
     }
 }
