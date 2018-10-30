23ca7b5ba4b67ce9e1fab141959763ac4cf7adb8
Daniel Profili <dprofili@gmail.com>
2018-10-30 07:56:37 -0400

    Finally fix recycler view

--- app/src/main/java/kantwonskids/donationtrackerg14b/controller/LocationListFragment.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/controller/LocationListFragment.java
@@ -1,29 +1,127 @@
 package kantwonskids.donationtrackerg14b.controller;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 import kantwonskids.donationtrackerg14b.R;
+import kantwonskids.donationtrackerg14b.model.Location;
+import kantwonskids.donationtrackerg14b.model.Model;
 
-public class LocationListFragment extends Fragment {
+public class LocationListFragment extends ListFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        View recyclerView = findViewById(R.id.location_list);
-//        assert recyclerView != null;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+//        View recyclerView = view.findViewById(R.id.location_list);
+////        assert recyclerView != null;
 //        setupRecyclerView((RecyclerView) recyclerView);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(
+                Model.getInstance().donationDataList));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        /**
+         * The items to be shown in the list
+         */
+        private final List<Location> mLocations;
+
+        /**
+         * Sets the items to be used by the adapter.
+         * @param locations
+         */
+        SimpleItemRecyclerViewAdapter(List<Location> locations) {
+            mLocations = locations;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.location_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            final Model model = Model.getInstance();
+            holder.mLocation = mLocations.get(position);
+            holder.mContentView.setText(mLocations.get(position).toString());
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context,
+                            LocationDetailActivity.class);
+                    //intent.putExtra(LocationDetailFragment.ARG_NAME,
+                    //      holder.mLocation.getName());
+                    model.setCurrentLocation(holder.mLocation);
+                    context.startActivity(intent);
+                }
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            if (mLocations != null) {
+                return mLocations.size();
+            } else {
+                return 0;
+            }
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mContentView;
+            public Location mLocation;
+
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+        }
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.location_list, container, false);
+
+        View rootView = inflater.inflate(R.layout.location_list, container, false);
+        View recyclerView = rootView.findViewById(R.id.location_list);
+//        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
     }
 }
