4a9fdd0abec36e8413bee8d3ffd340e880df4b10
Daniel Profili <dprofili@gmail.com>
2018-11-12 18:19:27 -0500

    Change user role slightly

--- app/src/main/java/kantwonskids/donationtrackerg14b/model/UserRole.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/model/UserRole.java
@@ -3,11 +3,11 @@ package kantwonskids.donationtrackerg14b.model;
 import java.io.Serializable;
 
 public enum UserRole implements Serializable {
-    GUEST("Guest", false, false, false, false, false),
-    USER("User", true, false, false, false, false),
-    LOCATION_EMPLOYEE("Location Employee", true, true, false, false, false),
-    MANAGER("Manager", true, true, false, false, false),
-    ADMINISTRATOR("Administrator", true, true, true, true, true);
+    GUEST("Guest", false, false, false, false, false, false),
+    USER("User", true, false, false, false, false, false),
+    LOCATION_EMPLOYEE("Location Employee", true, true, false, false, false, false),
+    MANAGER("Manager", true, true, true, false, false, false),
+    ADMINISTRATOR("Administrator", true, true, true, true, true, true);
 
     /*
      * Enumerates the permissions for each user type.
@@ -31,6 +31,7 @@ public enum UserRole implements Serializable {
      */
     private boolean canLogIn;
     private boolean canUpdateInfo;
+    private boolean canUpdateInfoAtAllLocations;
     private boolean canAddOrRemoveUsers;
     private boolean canLockOrUnlockUsers;
     private boolean canAddOrRemoveLocations;
@@ -41,16 +42,18 @@ public enum UserRole implements Serializable {
     /**
      * Constructor. Initializes all the permissions for a user type.
      * @param canLogIn Whether the user type can log in
-     * @param canUpdateInfo Whether the user type can update info at one or more locations
+     * @param canUpdateInfo Whether the user type can update info at a single location
+     * @param canUpdateInfoAtAllLocations whether the user type can update info at ALL locations
      * @param canAddOrRemoveUsers Whether the user type can add or remove users
      * @param canLockOrUnlockUsers Whether the user type can lock or unlock users
      * @param canAddOrRemoveLocations Whether the user type can add or remove locations
      */
-    UserRole(String name, boolean canLogIn, boolean canUpdateInfo, boolean canAddOrRemoveUsers,
-             boolean canLockOrUnlockUsers, boolean canAddOrRemoveLocations) {
+    UserRole(String name, boolean canLogIn, boolean canUpdateInfo, boolean canUpdateInfoAtAllLocations,
+             boolean canAddOrRemoveUsers, boolean canLockOrUnlockUsers, boolean canAddOrRemoveLocations) {
         this.name = name;
         this.canLogIn = canLogIn;
         this.canUpdateInfo = canUpdateInfo;
+        this.canUpdateInfoAtAllLocations = canUpdateInfoAtAllLocations;
         this.canAddOrRemoveUsers = canAddOrRemoveUsers;
         this.canLockOrUnlockUsers = canLockOrUnlockUsers;
         this.canAddOrRemoveLocations = canAddOrRemoveLocations;
