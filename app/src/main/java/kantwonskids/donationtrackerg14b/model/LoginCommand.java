a8ec77f7fc5a49c186cbf47de3ff500b5498c519
Daniel Profili <dprofili@gmail.com>
2018-12-03 18:15:32 -0500

    Login command skeleton

--- app/src/main/java/kantwonskids/donationtrackerg14b/model/LoginCommand.java
+++ app/src/main/java/kantwonskids/donationtrackerg14b/model/LoginCommand.java
@@ -4,5 +4,13 @@ package kantwonskids.donationtrackerg14b.model;
  * Represents a login command. Executed when a user wants to login.
  */
 public class LoginCommand implements Command {
+    private boolean doLogin() {
+        return false;
+    }
+
+    @Override
+    public boolean execute() {
+        return doLogin();
+    }
 
 }
